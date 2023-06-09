42fb9d2f4db594a1b6febaff19f57a1ff5b21044
OmerMalach <102660223+OmerMalach@users.noreply.github.com>
2023-06-09 10:44:21 +0300

    09/06/2023 update

--- studybuddy/db/db.js
+++ studybuddy/db/db.js
@@ -1,15 +1,37 @@
-const mysql = require("mysql2");
-const dbConfig = require("./db.config.js");
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const CRUD = require("./db/CRUD");
+const cookieParser = require("cookie-parser"); // to install.
+const app = express();
+const upload = multer({ dest: "uploads/" });
+
+const multer = require("multer");
+const { google } = require("googleapis");
+const { OAuth2Client } = require("google-auth-library");
+const fs = require("fs");
+const sql = require("./db");
+
+const port = 3000;
+app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "static")));
+app.use(cookieParser());
+
+// routing
+app.get("/", (req, res) => {
+  //res.json({ message: "Welcome to web course example application." });
+  res.sendFile(path.join(__dirname, "views/NewAccount.html"));
 });
-// open the MySQL connection
-connection.connect((error) => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
+
+// set port, listen for requests
+app.listen(port, () => {
+  console.log("Server is running on port:", port);
 });
-module.exports = connection;
+
+app.post("/signup", CRUD.createNewUser);
+
+app.post("/newpost", CRUD.createNewPost);
+
+app.get("/show", CRUD.showAll);
