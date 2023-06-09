0b99ddb551fe83723afb5dd32876851b25ae067e
amitrosen <rosenbaa@post.bgu.ac.il>
2023-06-09 20:12:50 +0300

    initial commit

--- studybuddy/index.js
+++ studybuddy/index.js
@@ -11,6 +11,80 @@ const { google } = require("googleapis");
 const { OAuth2Client } = require("google-auth-library");
 const fs = require("fs");
 const sql = require("./db");
+const CLIENT_ID: '400315462079-13dgnaqavlnficsh8saoq97pqbajfk99.apps.googleusercontent.com';
+const CLIENT_secret='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';
+const REDIRECT_URI= 'https:/developers.google.com/oauthplayground';
+const REFRESH_TOKEN= '1//04GZSwD6ILCZaCgYIARAAGAQSNwF-L9IrkNe0din7KTbV-TzOZY8IUu0ln-VV0IP7brmOLctjEb-2ENbv6Bjr-qB49Z6wc9BRD-o'
+
+const oauth2Client= new google.auth.oAuth2(
+CLIENT_ID,
+CLIENT_secret,
+REDIRECT_URI
+);
+oauth2Client.setCredentials({refresh_token: REFRESH_TOKEN})
+
+const drive= google.drive({
+version: 'v3',
+auto:oauth2Client
+})
+
+const filePath= path.join(__dirname, )
+
+async function uploadFile(){
+try{
+const response= await drive.files.create({
+requestBody: {
+name:
+mimeType:
+},
+media:{
+mimeType:
+body: fs.createReadStream(filePath)
+ }
+})
+ console.log(response.data)
+} catch(error){
+console.log(error.message)
+ }
+}
+
+uploadFile();
+
+
+async function deleteFile(){
+try{
+const response= await drive.files.delete({
+fileId :
+
+});
+console.log(response.data ,response.status)
+} catch (error){
+console.log(error.message);
+ }
+}
+deleteFile();
+
+async function generatePublic (){
+try{
+const fileId=
+await drive.permissions.create({
+fileId:
+requestBody:{
+role:'reader',
+type: 'anyone'
+ }
+})
+const result =await drive.files.get({
+fileId: fileId,
+fields: 'webViewLink' ,'webContentLink',
+});
+console.log.(result.data);
+
+} catch (error){
+console.log(error.message)
+  }
+}
+generatePublic ();
 
 const port = 3000;
 app.set("views", path.join(__dirname, "views"));
